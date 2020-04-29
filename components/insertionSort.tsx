62fe44e787c6cefc0a0a76f17de761d8369ae1ec
rainguy1 <rainguy1@saramin.co.kr>
2020-04-29 11:37:00 +0900

    잼있네

--- components/insertionSort.tsx
+++ components/insertionSort.tsx
@@ -2,12 +2,36 @@ import { range, shuffle } from 'lodash'
 import { useState } from 'react'
 
 const getArr = () => shuffle(range(1,11))
+const swap = (arr, a, b) => {
+  const tmp = arr[a]
+  arr[a] = arr[b]
+  arr[b] = tmp
+}
+const sort = (arr: number[]) => {
+  let i = 1;
+  while (i < arr.length) {
+    let j = i;
+    while(j > 0 && arr[j-1] > arr[j]) {
+      swap(arr, j, j -1)
+      j = j - 1
+    }
+    i = i + 1
+  }
+}
+
 export default () => {
   const [arr, setArr] = useState(getArr())
 
   const handleShuffled = () => {
     setArr(getArr())
   }
+  
+  const handleSort = () => {
+    const sortedArr = [...arr];
+    sort(sortedArr)
+    setArr(sortedArr)
+  }
+
 	return (
 		<div>
 			<h2 className="title">BOARD</h2>
@@ -16,7 +40,7 @@ export default () => {
 			</div>
 			<div className="wrap_btn">
 				<button type='button' className="btn" onClick={handleShuffled}>shuffle</button>
-				<button type='button' className="btn" onClick={handleShuffled}>sort</button>
+				<button type='button' className="btn" onClick={handleSort}>sort</button>
 			</div>
 			<style jsx>{`
 				.title	{
